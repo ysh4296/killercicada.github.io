6a1c92eaeb0dd4ce6e4f9fc6aeafd33897fe4797
ysh4296 <dbtmdgns4296@naver.com>
2020-11-26 23:17:15 +0900

    Update add.js

--- add.js
+++ add.js
@@ -4,8 +4,8 @@ para.textContent = 'You clicked the p!';
 document.body.appendChild(para);
 }
 
-const buttons = document.querySelectorAll('p');
+const ps = document.querySelectorAll('p');
 
 for(let i = 0 ; i < buttons.length ; i++){
-  buttons[i].addEventListener('click',createli);
+  ps[i].addEventListener('click',createli);
 }
