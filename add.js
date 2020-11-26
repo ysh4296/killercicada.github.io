4844219932562e8c53d9c748eb47a397543896c1
ysh4296 <dbtmdgns4296@naver.com>
2020-11-26 23:04:19 +0900

    Create add.js

---
+++ add.js
@@ -0,0 +1,11 @@
+function createParagraph(){
+let para = document.createElement('li');
+para.textContent = 'You clicked the ul!';
+document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll('ul');
+
+for(let i = 0 ; i < buttons.length ; i++){
+  buttons[i].addEventListener('click',createParagraph);
+}
