ed8ea284ee59b0ae0eb8d6ded328c2677f6da68c
ysh4296 <dbtmdgns4296@naver.com>
2020-11-26 22:16:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function createParagraph(){
-let para = document.createElement('ul');
+let para = document.createElement('p');
 para.textContent = 'You clicked the button!';
 document.body.appendChild(para);
 }
 
-const buttons = document.querySelectorAll('ul');
+const buttons = document.querySelectorAll('li');
 
 for(let i = 0 ; i < buttons.length ; i++){
   buttons[i].addEventListener('click',createParagraph);
