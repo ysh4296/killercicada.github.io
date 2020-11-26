3c80c8ae85c437a8cc0abc3bbbb3e601baa74aed
ysh4296 <dbtmdgns4296@naver.com>
2020-11-26 23:10:14 +0900

    Update add.js

--- add.js
+++ add.js
@@ -1,4 +1,4 @@
-function createParagraph(){
+function create(){
 let para = document.createElement('li');
 para.textContent = 'You clicked the ul!';
 document.body.appendChild(para);
@@ -7,5 +7,5 @@ document.body.appendChild(para);
 const buttons = document.querySelectorAll('ul');
 
 for(let i = 0 ; i < buttons.length ; i++){
-  buttons[i].addEventListener('click',createParagraph);
+  buttons[i].addEventListener('click',create);
 }
