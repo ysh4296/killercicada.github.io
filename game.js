52cc769cfbd8ea21067023c02309a31e9ee2d446
ysh4296 <dbtmdgns4296@naver.com>
2020-12-21 23:44:37 +0900

    Update game.js

--- game.js
+++ game.js
@@ -8,3 +8,31 @@ var guessField = document.querySelector('.guessField');
 
 var guessCount = 1;
 vat resetButton;
+
+function checkGuess(){
+  var userGuess = Number(guessField.value);
+  if(guessCount===1){
+    guesses.textContent = 'Previous guesses: ';
+  }
+  guesses.textContent += userGuess + ' ';
+  if(userGuess === randomNumber){
+    lastResult.textContent = 'Congratulations! You got it right!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent =' ';
+    setGameOver();
+  } else if (guessCount === 10){
+    lastResult.textContent = '!!!GAMEOVER!!!;
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+    if(userGuess < randomNumber){
+      lowOrHi.textContent = 'last guess was too low';
+    } else {
+      lowOrHi.textContent = 'last guess was too high';
+    }
+  }  
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+}
