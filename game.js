5c115eb26aca01d21a042eb047d6739b1ea0b0d9
ysh4296 <dbtmdgns4296@naver.com>
2020-12-21 23:22:08 +0900

    Create game.js

---
+++ game.js
@@ -0,0 +1,10 @@
+var randomNumber = Math.floor(Math.random()*100) + 1;
+var gueses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult');
+var lowOrHi = document.querySelector('.lowOrHi');
+
+var guessSubmit = document.querySelector('.guessSubmit');
+var guessField = document.querySelector('.guessField');
+
+var guessCount = 1;
+vat resetButton;
